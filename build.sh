ff5ac111ecadb921e6606967831b6598b1899cac
kulisdominik <32802894+kulisdominik@users.noreply.github.com>
2018-03-11 12:20:40 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,10 +15,6 @@ dotnet test ./test/MedicalClinic -c Release -f netcoreapp1.0
 
 dotnet build ./test/MedicalClinic-c Release -f net451
 
-mono \  
-./test/MedicalClinic/bin/Release/net451/*/dotnet-test-xunit.exe \
-./test/MedicalClinic/bin/Release/net451/*/TEST_PROJECT_NAME.dll
-
 revision=${TRAVIS_JOB_ID:=1}  
 revision=$(printf "%04d" $revision) 
 
