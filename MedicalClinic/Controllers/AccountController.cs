eb71efe0ef67d7bbffb2d2e28532113249cb56f9
kulisdominik <kulisdominikgit@gmail.com>
2018-05-03 16:28:28 +0200

    change defoult migration folder

--- MedicalClinic/Controllers/AccountController.cs
+++ MedicalClinic/Controllers/AccountController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MedicalClinic.Models;
 using MedicalClinic.Models.AccountViewModels;
 using Microsoft.AspNetCore.Authentication;
