debad9c08f867caa68642e8853049ca9fb28b6f3
kulisdominik <kulisdominikgit@gmail.com>
2018-05-06 16:45:00 +0200

    Add admin panel

---
+++ MedicalClinic/Controllers/AdminController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MedicalClinic.Data.Migrations;
+using MedicalClinic.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalClinic.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private ApplicationDbContext _context;
+
+        public AdminController(
+            UserManager<ApplicationUser> userManager,
+            ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult EditUser()
+        {
+            return View(_context.Users.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            var user = _context.Users.SingleOrDefault(u => u.Id == id);
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, ApplicationUser user)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Update(user);
+                await _context.SaveChangesAsync();
+            }
+
+            return View(user);
+        }
+
+        public IActionResult Details(string id)
+        {
+            var user = _context.Users.SingleOrDefault(u => u.Id == id);
+            return View(user);
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(string id)
+        {
+            var user = _context.Users.SingleOrDefault(u => u.Id == id);
+            return View(user);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var movie = await _context.Users.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Users.Remove(movie);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("EditUser");
+        }
+    }
+}
