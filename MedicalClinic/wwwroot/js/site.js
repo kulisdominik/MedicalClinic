b186bcbb3d67d79c54a0588fe5b2379ddc37a519
Iwona Jeleń <iwoj345@gmail.com>
2018-05-09 12:19:11 +0200

    new layout (2)

--- MedicalClinic/wwwroot/js/site.js
+++ MedicalClinic/wwwroot/js/site.js
@@ -1,30 +1,9 @@
 ﻿$(function () {
-    let menuIsVisible = true;
-
-    if (window.matchMedia('(max-width: 992px)').matches) {
-        menuIsVisible = false;
-    }
-
     $('.btn-menu-slide').on('click', function () {
-        if (menuIsVisible) {
-            $(this).parent().animate({ right: '-250px' }, 400);
-            $(this).animate({ right: '0' }, 400);
-            $content = $('.content article');
-            $content.addClass('col-md-12');
-            $content.removeClass('col-md-10');
-            menuIsVisible = false;
-        }
-        else {
-            $(this).parent().animate({ right: '0' }, 400);
-            $(this).animate({ right: '250px' }, 400);
-            $content = $('.content article');
-            $content.addClass('col-md-10');
-            $content.removeClass('col-md-12');
-            menuIsVisible = true;
-        }
+     $('.menu').slideToggle();
     });
 
     $('.dropdown').on('click', function () {
         $(this).next().slideToggle();
-    })
+    });
 })
