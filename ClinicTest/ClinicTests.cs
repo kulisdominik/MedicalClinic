1612a9b9542947926f6516b933b08114bf2351c3
ravpawlisz <ravpawlisz@gmail.com>
2018-05-14 00:08:58 +0200

    empty test

---
+++ ClinicTest/ClinicTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MedicalClinic;
+
+namespace ClinicTest
+{
+    [TestClass]
+    public class ClinicTests
+    {
+        [TestMethod]
+        public void TestSomething()
+        {
+            //arrange
+            //declare variables to test
+
+            //act
+            //run testing method
+
+            //assert
+            //check if values are proper
+        }
+    }
+}
---
+++ ClinicTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClinicTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ClinicTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0b032df-41ff-4cf1-b747-063d6b60c514")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
