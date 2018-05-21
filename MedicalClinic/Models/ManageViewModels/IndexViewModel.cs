0887d485a0b7c0b5af48f04f3b591a0b50c07bf5
Weronika Mirek <werussia16@gmail.com>
2018-05-21 23:17:31 +0200

    added more fields in youraccount (editing) and some styles

--- MedicalClinic/Models/ManageViewModels/IndexViewModel.cs
+++ MedicalClinic/Models/ManageViewModels/IndexViewModel.cs
@@ -11,15 +11,45 @@ namespace MedicalClinic.Models.ManageViewModels
         [Display(Name = "Nazwa użytkownika")]
         public string Username { get; set; }
 
+        [Display(Name = "Imię")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Nazwisko")]
+        public string LastName { get; set; }
+
+        [Display(Name = "PESEL")]
+        public string PIN { get; set; }
+
+        [Phone]
+        [Display(Name = "Numer telefonu")]
+        public string PhoneNumber { get; set; }
+
         public bool IsEmailConfirmed { get; set; }
 
         [Required(ErrorMessage = "E-mail jest wymagany.")]
         [EmailAddress]
         public string Email { get; set; }
 
-        [Phone]
-        [Display(Name = "Numer telefonu")]
-        public string PhoneNumber { get; set; }
+        [Display(Name = "Płeć")]
+        public string Sex { get; set; }
+
+        [Display(Name = "Kraj")]
+        public string Country { get; set; }
+
+        [Display(Name = "Ulica")]
+        public string Street { get; set; }
+
+        [Display(Name = "Miasto")]
+        public string City { get; set; }
+
+        [Display(Name = "Kod pocztowy")]
+        public string PostalCode { get; set; }
+
+        [Display(Name = "Numer budynku")]
+        public string BuildingNum { get; set; }
+
+        [Display(Name = "Numer mieszkania")]
+        public string FlatNum { get; set; }
 
         public string StatusMessage { get; set; }
     }
